974502d5a3d860a6e468310cff0ac78522963b4e
xrosilloprofe <xv.rosillo@edu.gva.es>
2024-12-05 19:01:36 +0100

    20241205 v0

--- src/ejercicios8y9/Geometria.java
+++ src/ejercicios8y9/Geometria.java
@@ -2,7 +2,13 @@ package ejercicios8y9;
 
 public class Geometria {
     public static void main(String[] args) {
+        Punto origen = new Punto(5,5);
+        System.out.println(origen);
+        Punto destino = origen.nuevoPuntoArriba(10);
+        destino.moverIzquierda(5);
 
+        System.out.println(destino);
+        System.out.println(destino.distancia(origen));
 
     }
 }
--- src/ejercicios8y9/Punto.java
+++ src/ejercicios8y9/Punto.java
@@ -21,6 +21,40 @@ public class Punto {
     public int getY(){return y;}
 
     //métodos funcionales
+    public void moverArriba(int distancia){
+        this.y += distancia;
+    }
+    public void moverAbajo(int distancia){
+        this.y -= distancia;
+    }
+    public void moverDerecha(int distancia){
+        this.x += distancia;
+    }
+    public void moverIzquierda(int distancia){
+        this.x -= distancia;
+    }
+
+    public Punto nuevoPuntoArriba(int distancia){
+        return new Punto(x,y+distancia);
+    }
+    public Punto nuevoPuntoAbajo(int distancia){
+        return new Punto(this.x,this.y-distancia);
+    }
+    public Punto nuevoPuntoIzquierda(int distancia){
+        return new Punto(this.x-distancia,this.y);
+    }
+    public Punto nuevoPuntoDerecha(int distancia){
+        return new Punto(this.x+distancia,this.y);
+    }
+
+    public double distancia(Punto puntoDestino){
+        return Math.sqrt(Math.pow(puntoDestino.getX()-this.x,2)+
+                Math.pow(puntoDestino.getY()-this.y,2));
+    }
 
     //sobreescribir métodos Object
+    @Override
+    public String toString(){
+        return "("+x+","+y+")";
+    }
 }
