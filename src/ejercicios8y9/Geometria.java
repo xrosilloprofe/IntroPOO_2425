301c0ca4e9b88b29ce366359927f5658aa2b1bfc
xrosilloprofe <xv.rosillo@edu.gva.es>
2024-12-05 18:35:36 +0100

    20241205 v0

--- src/ejercicios8y9/Geometria.java
+++ src/ejercicios8y9/Geometria.java
@@ -3,5 +3,6 @@ package ejercicios8y9;
 public class Geometria {
     public static void main(String[] args) {
 
+
     }
 }
---
+++ src/ejercicios8y9/Punto.java
@@ -0,0 +1,26 @@
+package ejercicios8y9;
+
+public class Punto {
+    //atributos
+    private int x;
+    private int y;
+
+    //constructor
+    public Punto(){
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Punto(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    //getters
+    public int getX(){return x;}
+    public int getY(){return y;}
+
+    //métodos funcionales
+
+    //sobreescribir métodos Object
+}
