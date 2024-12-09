2e0f59b83d3366d2ed2125df8fb1244ba8595972
xrosilloprofe <xv.rosillo@edu.gva.es>
2024-12-09 19:08:07 +0100

    20241209 v0.1

--- src/ejercicios8y9/Geometria.java
+++ src/ejercicios8y9/Geometria.java
@@ -2,10 +2,10 @@ package ejercicios8y9;
 
 public class Geometria {
     public static void main(String[] args) {
-        Punto origen = new Punto(5,5);
+        Punto origen = new Punto(25,25);
         System.out.println(origen);
         Punto destino = origen.nuevoPuntoArriba(10);
-        destino.moverIzquierda(5);
+        destino.moverDerecha(5);
 
         System.out.println(destino);
         System.out.println(destino.distancia(origen));
@@ -13,6 +13,14 @@ public class Geometria {
         Segmento linea = new Segmento(origen,destino);
         linea.moverIzquierda(20);
         System.out.println(linea);
+
+        Rectangulo rectangulo1 = new Rectangulo(origen, destino);
+        System.out.println(rectangulo1.obtenerPerimetro());
+        System.out.println(rectangulo1);
+
+        Rectangulo rectangulo2 = new Rectangulo(destino,50,50);
+        System.out.println(rectangulo2.obtenerPerimetro());
+        System.out.println(rectangulo2);
 
     }
 }
--- src/ejercicios8y9/Rectangulo.java
+++ src/ejercicios8y9/Rectangulo.java
@@ -2,14 +2,46 @@ package ejercicios8y9;
 
 public class Rectangulo {
     //Cuatro puntos
+    private Punto inferiorDerecha;
+    private Punto inferiorIzquierda;
+    private Punto superiorDerecha;
+    private Punto superiorIzquierda;
 
     //Constructor a partir de un punto inferiorIzda y un punto superior derecha
+    public Rectangulo(Punto inferiorIzquierda, Punto superiorDerecha){
+        this.inferiorIzquierda = inferiorIzquierda;
+        this.superiorDerecha = superiorDerecha;
+        this.superiorIzquierda =
+                inferiorIzquierda.nuevoPuntoArriba(superiorDerecha.getY()- inferiorIzquierda.getY());
+        this.inferiorDerecha = new Punto(superiorDerecha.getX(), inferiorIzquierda.getY());
+    }
 
     //constructor a partir de punto inferior izquierda, base y altura
+    public Rectangulo(Punto inferiorIzquierda, int base, int altura){
+        this.inferiorIzquierda = inferiorIzquierda;
+        this.inferiorDerecha = inferiorIzquierda.nuevoPuntoDerecha(base);
+        this.superiorIzquierda = new Punto(inferiorIzquierda.getX(), inferiorIzquierda.getY()+altura);
+        this.superiorDerecha = inferiorDerecha.nuevoPuntoArriba(altura);
+    }
 
     // métodos para obtener base y altura
+    public int obtenerBase(){
+        return (int)(superiorIzquierda.distancia(superiorDerecha));
+    }
+
+    public int obtenerAltura(){
+        return (int)(superiorIzquierda.distancia(inferiorIzquierda));
+    }
 
     //método para calcular el perímetro
+    public int obtenerPerimetro(){
+        return 2*(obtenerAltura()+obtenerBase());
+    }
 
     //sobreescribir toString
+    @Override
+    public String toString(){
+        return inferiorIzquierda.toString() + " " + inferiorDerecha.toString() + " "
+                + superiorIzquierda.toString() + " " + superiorDerecha.toString();
+    }
 }
